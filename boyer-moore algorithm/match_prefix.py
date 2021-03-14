deed34145b769f52cdc370b0f50e014d20a4c05d
shun <songshunnew@hotmail.com>
2021-03-14 21:40:26 +0800

    match prefix

---
+++ boyer-moore algorithm/match_prefix.py
@@ -0,0 +1,14 @@
+import z_algo
+
+def match_prefix(pat):
+    z_array = z_algo.z_algo(pat)
+    mp_array = [0] * (len(pat) + 1)
+    for i in range(len(pat)-1, -1, -1):
+        if z_array[i] + i == len(pat):
+            mp_array[i] = z_array[i]
+        else:
+            mp_array[i] = mp_array[i+1]
+    return mp_array
+
+pat = "aabaabcaxaabaabcy"
+print(match_prefix(pat))
